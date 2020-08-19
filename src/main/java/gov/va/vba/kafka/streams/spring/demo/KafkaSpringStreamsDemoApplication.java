c7259f9a404aa66d2341c2653c3532b69eb40fd9
atimes <582309@bah.com>
2020-08-19 12:46:51 -0400

    Old hello world program before it gets gutted for the "new" stuff

--- src/main/java/gov/va/vba/kafka/streams/spring/demo/KafkaSpringStreamsDemoApplication.java
+++ src/main/java/gov/va/vba/kafka/streams/spring/demo/KafkaSpringStreamsDemoApplication.java
@@ -9,5 +9,4 @@ public class KafkaSpringStreamsDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaSpringStreamsDemoApplication.class, args);
 	}
-
 }
---
+++ src/main/java/gov/va/vba/kafka/streams/spring/demo/config/KafkaConfig.java
@@ -0,0 +1,57 @@
+package gov.va.vba.kafka.streams.spring.demo.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.consumer.topic-name}")
+    private String inputTopic;
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    Logger logger = LoggerFactory.getLogger(KafkaConfig.class);
+
+    @Bean
+    public KafkaStreams configureKafka() {
+        Properties streamsConfiguration = new Properties();
+
+        //ensure the stream always start at the beginning
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, UUID.randomUUID().toString());
+        streamsConfiguration.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.String().getClass().getName());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> lines = builder.stream(inputTopic);
+
+        lines.foreach((key, value) -> logger.info("Value:" + value));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
+        streams.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+        return streams;
+    }
+}
