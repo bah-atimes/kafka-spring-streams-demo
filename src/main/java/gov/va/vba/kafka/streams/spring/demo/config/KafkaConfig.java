2c0b2296aee2103cda925399119ef7d441f732dd
atimes <582309@bah.com>
2020-08-19 15:48:39 -0400

    1st pass at bringing in new stuff.  Not done completely yet.

--- src/main/java/gov/va/vba/kafka/streams/spring/demo/config/KafkaConfig.java
+++ src/main/java/gov/va/vba/kafka/streams/spring/demo/config/KafkaConfig.java
@@ -1,57 +1,108 @@
 package gov.va.vba.kafka.streams.spring.demo.config;
 
+import gov.va.vba.kafka.streams.spring.demo.processors.ClaimMessageProcessor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 
 @Configuration
 public class KafkaConfig {
 
-    @Value("${spring.kafka.consumer.topic-name}")
-    private String inputTopic;
-
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    Logger logger = LoggerFactory.getLogger(KafkaConfig.class);
+    @Value("${spring.kafka.consumer.topic-name}")
+    private String inputTopic;
+
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String consumerAutoOffsetReset;
+
+    @Value("${spring.kafka.consumer.batch-message-count}")
+    private Integer batchMessageCount;
+
+    @Value("${spring.kafka.consumer.wait-interval-ms}")
+    private Integer waitInterval;
+
+    private String processorClassName = ClaimMessageProcessor.class.getName();
+
+    private Logger logger = LoggerFactory.getLogger(KafkaStreams.class);
+
+    private static final String OUTPUT_TOPIC = "streams-chunked-output";
+    private static final String SOURCE_NAME = "SOURCE";
+    private static final String SINK_NAME = "SINK";
+
 
     @Bean
     public KafkaStreams configureKafka() {
-        Properties streamsConfiguration = new Properties();
-
-        //ensure the stream always start at the beginning
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, UUID.randomUUID().toString());
-        streamsConfiguration.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
-        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
-        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.String().getClass().getName());
 
         StreamsBuilder builder = new StreamsBuilder();
         KStream<String, String> lines = builder.stream(inputTopic);
 
         lines.foreach((key, value) -> logger.info("Value:" + value));
 
-        KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
+        //KafkaStreams streams = new KafkaStreams(builder.build(), getStreamConfiguration());
+        KafkaStreams streams = new KafkaStreams(getTopology(), getStreamConfiguration());
+        streams.cleanUp();
         streams.start();
 
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
 
         return streams;
+    }
+
+    private Properties getStreamConfiguration() {
+        final Properties streamsConfiguration = new Properties();
+
+        //ensure the stream always start at the beginning
+        streamsConfiguration.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerAutoOffsetReset);
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, UUID.randomUUID().toString());
+        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+        streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, "kafka-stream-store/" + UUID.randomUUID().toString());
+        //streamsConfiguration.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+
+        return streamsConfiguration;
+    }
+
+    private Topology getTopology() {
+        final Topology topology = new Topology();
+        topology.addSource(SOURCE_NAME, new StringDeserializer(), new StringDeserializer(), inputTopic);
+        topology.addProcessor(processorClassName, () -> new ClaimMessageProcessor(batchMessageCount, waitInterval), SOURCE_NAME);
+        topology.addStateStore(getStoreBuilder());
+        topology.connectProcessorAndStateStores(processorClassName, processorClassName);
+        topology.addSink(SINK_NAME, OUTPUT_TOPIC, new StringSerializer(), new StringSerializer(), processorClassName);
+        return topology;
+    }
+
+
+    private StoreBuilder<KeyValueStore<Long, String>> getStoreBuilder() {
+        return Stores.keyValueStoreBuilder(
+//                Stores.persistentKeyValueStore(processorClassName),
+                Stores.inMemoryKeyValueStore(processorClassName),
+                Serdes.Long(),
+                Serdes.String());
     }
 }
---
+++ src/main/java/gov/va/vba/kafka/streams/spring/demo/processors/ClaimMessageProcessor.java
@@ -0,0 +1,140 @@
+package gov.va.vba.kafka.streams.spring.demo.processors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.processor.Processor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.PunctuationType;
+import org.apache.kafka.streams.processor.Punctuator;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+public class ClaimMessageProcessor implements Processor<Long, String> {
+
+    private int batchSize;
+    private int waitTime;
+
+    private KeyValueStore<Long, String> keyValueStore;
+    private ProcessorContext processorContext = null;
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final AtomicLong aggregatedClaimCount = new AtomicLong(0L);
+    private final AtomicLong messageCount = new AtomicLong(0L);
+
+    public ClaimMessageProcessor(int batchSize, int waitTime) {
+        this.batchSize = batchSize;
+        this.waitTime = waitTime;
+    }
+
+    private Logger logger = LoggerFactory.getLogger(ClaimMessageProcessor.class);
+
+    @Override
+    @SuppressWarnings("unchecked") // need specific type of state store
+    public void init(final ProcessorContext processorContext) {
+        this.processorContext = processorContext;
+        keyValueStore = (KeyValueStore<Long, String>) processorContext.getStateStore(ClaimMessageProcessor.class.getName());
+        processorContext.schedule(
+                Duration.ofMillis(waitTime), PunctuationType.WALL_CLOCK_TIME, getPunctuator());
+    }
+
+    /**
+     * This is the method that handles the state store after {@value {spring.kafka.consumer.wait-interval-ms}} seconds.  The goal is to commit
+     * {@value {spring.kafka.consumer.batch-message-count}} values as a list in JSON format and delete those values from the store.
+     *
+     * @return A {@link Punctuator}.
+     */
+    private Punctuator getPunctuator() {
+        return (timestamp) -> {
+            handleKeyValues(getKeyValueData().iterator());
+            keyValueStore.flush();
+            processorContext.commit();
+        };
+    }
+
+    /**
+     * Schedules a delete from the store and adds the value to a list of strings to forward.  If the list is size
+     * {@value {spring.kafka.consumer.batch-message-count}} or there's no more records in the store, then forward on a list as a JSON string
+     * of the values.
+     *
+     * @param iterator The {@link KeyValueIterator} we're getting from the store.
+     */
+    private void handleKeyValues(final Iterator<KeyValue<Long, String>> iterator) {
+        final List<String> strings = new ArrayList<>();
+        while (iterator.hasNext()) {
+            final KeyValue<Long, String> keyValue = iterator.next();
+            keyValueStore.delete(keyValue.key);
+            strings.add(keyValue.value);
+            if (strings.size() == batchSize || !iterator.hasNext()) {
+                //processorContext.forward(Long.toString(messageCount.incrementAndGet()), getJsonData(strings));
+                processTopicMessagesInBatch(strings);
+                strings.clear();
+            }
+        }
+    }
+
+    private void processTopicMessagesInBatch(List<String> messages) {
+        logger.info("got:" + messages.size() + " claims from topic");
+        for(String message : messages) {
+            processorContext.forward(Long.toString(aggregatedClaimCount.incrementAndGet()), message);
+        }
+    }
+
+    /**
+     * Gets the data from the store, then sorts it based on timestamp.
+     *
+     * @return a list of key values sorted by timestamp
+     */
+    private List<KeyValue<Long, String>> getKeyValueData() {
+        final KeyValueIterator<Long, String> iterator = keyValueStore.all();
+        final List<KeyValue<Long, String>> keyValues = new ArrayList<>();
+        while (iterator.hasNext()) {
+            keyValues.add(iterator.next());
+        }
+        iterator.close();
+        return keyValues.stream()
+                .sorted(Comparator.comparingLong(e -> e.key))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Converts a list of strings to JSON.
+     *
+     * @param strings The strings.
+     * @return The JSON string.
+     */
+    private String getJsonData(final List<String> strings) {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            mapper.writeValue(out, strings);
+        } catch (final IOException e) {
+            throw new RuntimeException("error trying to parse json!", e);
+        }
+        return new String(out.toByteArray());
+    }
+
+    /**
+     * Breaks line of text into words, then gives a key value for each word.
+     * @param key for the record, but not used in this context
+     * @param line the line of text
+     */
+    @Override
+    public void process(final Long key, final String line) {
+        keyValueStore.put(messageCount.incrementAndGet(), line);
+    }
+
+    @Override
+    public void close() {
+        //nothing to do
+    }
+}
